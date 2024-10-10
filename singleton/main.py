495408280a5b88b91c1519c913198944a8e02039
suparman <tegesitt@gmail.com>
2024-10-10 07:30:19 +0700

    singleton

--- singleton/main.py
+++ singleton/main.py
@@ -1,17 +1,90 @@
-class Singleton:
-    """
-    contoh singleton
-    """
-    _instance = None
+"""
+Implementasi singleton :
+1. Menggunakan metode metaclass
+2. Menggunakan fungsi dekorator
+3. Menggunakan modul
+"""
 
-    def __new__(cls):
-        if cls._instance is None:
-            cls._instance = super(Singleton, cls).__new__(cls)
+
+class SingletonWighMetaclass(type):
+    """
+    Menggunakan metaclass
+    """
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(
+                SingletonWighMetaclass, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+
+class Logger(metaclass=SingletonWighMetaclass):
+    """
+    subclass
+    """
+
+    def log(self, message):
+        """
+        log 
+        """
+        print(message)
+
+
+logger1 = Logger()
+logger2 = Logger()
+
+# logger1 dan logger2 adalah objek yang sama
+print(logger1 is logger2)  # Output: True
+
+#====================================
+
+def singleton_decorator(cls):
+    """
+    decorator singleton
+    """
+    instances = {}
+
+    def getinstance(*args, **kwargs):
+        if cls not in instances:
+            instances[cls] = cls(*args, **kwargs)
+        return instances[cls]
+    return getinstance
+
+
+@singleton_decorator
+class Database:
+    """
+    menggunakan decorator singleton
+    """
+
+    def __init__(self):
+        print("Connecting to database...")
+
+
+db1 = Database()
+db2 = Database()
+
+# db1 dan db2 adalah objek yang sama
+print(db1 is db2)  # Output: True
+
+#=============================
+class _SingletonModule:
+    _instance = None
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super(_SingletonModule, cls).__new__(cls, *args, **kwargs)
         return cls._instance
 
+class Database1(_SingletonModule):
+    """
+    singleton with modul
+    """
+    def __init__(self):
+        print("Connecting to database...")
 
-# Contoh penggunaan:
-singleton1 = Singleton()
-singleton2 = Singleton()
+db1 = Database1()
+db2 = Database1()
 
-print(singleton1 is singleton2)  # Output: True
+# db1 dan db2 adalah objek yang sama
+print(db1 is db2)  # Output: True
--- singleton/readme.md
+++ singleton/readme.md
@@ -1,10 +1,17 @@
+#Singleton
+
 Apa itu Singleton?
 Singleton adalah pola desain creational yang memastikan hanya ada satu instance (objek) dari suatu kelas selama program berjalan. Ini berguna ketika kita hanya membutuhkan satu objek tunggal untuk mengelola sumber daya atau konfigurasi tertentu.
 
 Contoh Kasus Penggunaan:
 
-Koneksi database: Hanya perlu ada satu koneksi database untuk seluruh aplikasi.
-Logger: Satu objek logger untuk mencatat semua aktivitas aplikasi.
-Konfigurasi: Satu objek konfigurasi untuk menyimpan semua pengaturan aplikasi.
-Implementasi Singleton di Python
-Ada beberapa cara untuk mengimplementasikan Singleton di Python. Berikut adalah beberapa metode yang umum digunakan:
+*Koneksi database: Hanya perlu ada satu koneksi database untuk seluruh aplikasi.
+*Logger: Satu objek logger untuk mencatat semua aktivitas aplikasi.
+*Konfigurasi: Satu objek konfigurasi untuk menyimpan semua pengaturan aplikasi.
+
+Kapan Menggunakan Singleton?
+Meskipun Singleton memiliki kegunaan, ada beberapa hal yang perlu dipertimbangkan sebelum menggunakannya:
+
+*Testabilitas: Singleton dapat membuat kode sulit untuk diuji karena ketergantungan global.
+*Ketergantungan: Penggunaan Singleton yang berlebihan dapat membuat kode menjadi sangat terkait satu sama lain.
+*Multithreading: Dalam lingkungan multithreading, Singleton mungkin memerlukan mekanisme locking untuk menghindari masalah konkurensi.
